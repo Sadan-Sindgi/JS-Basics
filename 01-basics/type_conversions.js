10f9d2483c7d05d06bbab88fb6f4447204af554c
Sadan-Sindgi <sindgisadanjs@gmail.com>
2025-01-12 13:38:40 +0000

    data conversions

--- 01-basics/type_conversions.js
+++ 01-basics/type_conversions.js
@@ -1,5 +1,19 @@
-let score = undefined;
-let conv_score = Number(score);
+// let score = undefined;
+// let conv_score = Number(score);
 
-console.log(typeof score);
-console.log(typeof conv_score);
+// console.log(typeof score);
+// console.log(typeof conv_score);
+// console.log(conv_score);
+
+// let temp_var = undefined;
+// let conv_boolean = Boolean(temp_var);
+
+// console.log(conv_boolean);
+
+
+let temp_var = 33;
+let conv_str = String(temp_var);
+
+console.log(conv_str);
+console.log(typeof conv_str);
+
